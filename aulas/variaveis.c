97c1e9c205e099a1a894927c7f5b9af5d719ff59
leozika04 <142408673+leozika04@users.noreply.github.com>
2023-08-30 13:43:11 +0000

    codigo da aula

---
+++ aulas/variaveis.c
@@ -0,0 +1,26 @@
+#include <stdio. h> 
+
+int main() { 
+char caracter = 0;// ou '\0' '\n', '\t' ; // 'a', 'b', 'c', -128 a 127 - 1 byte
+  
+ 
+unsidned char caracter_sem_sinal; //0 a 225  
+
+char i;
+char j;  
+
+char string[10]; // "um texto"
+
+int inteiro; // -2147483648 a 2147583647 -4 bytes 
+
+unsigned int inteiro_sem_sinal; // 0 a 4294967295
+
+short int inteiro_curto; // -32768 a 32767- 2 bytes
+
+unsigned short inteiro_curso_sem_sinal; // 0 a 225  
+
+float flutuante; // 0.123456f - 4 bytes 
+
+double duplo; // 0.123456789012345 - 8 bytes 
+
+void  
---
+++ praticas/pratica01/questao02.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+int main (){ 
+
+unsigned int base = 2;
+unsigned int altura = 100;
+
+float area = base * altura / 2;
+
+  printf("a area é %f\n" , area);
+    
+return 0;
+
+} 
+  
